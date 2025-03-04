e7b956c81eb2b7f01a2a017693ef681f7c0be497
Harsh Tanwar <harsh.tanwar9818@gmail.com>
2025-03-04 22:46:06 +0530

    complete project 2

--- Project2/index.js
+++ Project2/index.js
@@ -17,8 +17,16 @@ app.get("/", (req, res) => {
     })
 });
 
-app.post('create', (req,res) => {
-    
+app.post('/create', (req,res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, function(err){})
+    res.redirect("/")
+});
+
+app.get('/file/:filename', (req,res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+        
+    })
 })
 
 app.listen(3000, () => {
