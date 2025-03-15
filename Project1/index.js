a62b3bab745927101bd206ba0c701e6886883896
Harsh Tanwar <harsh.tanwar9818@gmail.com>
2025-03-15 11:29:58 +0530

    Revisit

--- Project1/index.js
+++ Project1/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.json());  //setting up parsers
+app.use(express.json());  //setting up parsers 
 app.use(express.urlencoded({extended: true})); //setting up parsers
+//Without above two lines, your app wouldn’t understand the data sent in requests properly. 
 
 app.use(express.static(path.join(__dirname, 'public')));  //path.join means(__dirmane + public) __dirname contanin path of our project means project 1's path i.e, user/harsh/d/practice/project1 & this line use to access static files like vanilla js , css, images
 
@@ -13,9 +14,9 @@ app.get("/", (req, res) => {
     res.render("index")  //ejs file render krwa rhe hai direct
 });
 
-app.get("/profile/:username", (req,res) => {  //: column se ab username dynamic ho gaya hai means ab hum name ki jgha kuch bhi likhenge to run hoga wo route
+app.get("/profile/:username", (req,res) => {  //: colun se ab username dynamic ho gaya hai means ab hum username ki jgha kuch bhi likhenge to run hoga wo route
 
-    req.params.username //jis bhi naam ke aage colon : hota hai usko hum params bolte hai
+    req.params.username //jis bhi naam ke aage colon(:) hota hai usko hum params bolte hai
     // res.send("Dynamic routing work");
     res.send(`Welcome ${req.params.username}`)
 })
