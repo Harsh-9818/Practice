f3baa3107fe39997e4d674fad0a4a14b318c9c8d
Harsh Tanwar <harsh.tanwar9818@gmail.com>
2025-03-15 11:36:50 +0530

    Revisit

--- Project2/index.js
+++ Project2/index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs')
